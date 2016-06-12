35d8070474507f7f197aec50f90b8140a98d925f
caraclarke <cara.clarke8@gmail.com>
2016-06-12 14:03:56 -0400

    test rendering

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>tesT</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
