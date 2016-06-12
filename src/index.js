885041c227b6907a0175d129358f0494e8ce2a8a
caraclarke <cara.clarke8@gmail.com>
2016-06-12 14:10:06 -0400

    added youtube api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('dotenv').config();
+
+const API_KEY = process.env.YOUTUBE_KEY;
+
 const App = () => {
   return <div>tesT</div>
 }
