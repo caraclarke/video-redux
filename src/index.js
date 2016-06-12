ae7258544b6879020cd74e23b7a056bf93e22c03
caraclarke <cara.clarke8@gmail.com>
2016-06-12 15:01:43 -0400

    update searchbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Searchbar from './components/search_bar';
 
-require('dotenv').config();
-
-const API_KEY = process.env.YOUTUBE_KEY;
+const API_KEY = process.env.API_KEY;
 
 const App = () => {
-  return <div>tesT</div>
+  return (
+    <div>
+      <Searchbar />
+    </div>
+  );
 }
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  node: {
+       fs: "empty"
+   },
   module: {
     loaders: [{
       exclude: /node_modules/,
